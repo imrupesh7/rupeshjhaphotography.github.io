414faac5b2d8f4187b4a6c5637871e4091333251
Rupesh Jha <101627025+imrupesh7@users.noreply.github.com>
2022-03-15 14:25:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+
+
+function gonext(){
+    window.location="quiz1.html"
+}
+
+function home(){
+    window.location="index.html"
+}
+
+function next(from){
+    if(from.test [0].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz1.html";
+    }
+
+    if( from.test[1].checked){
+        alert("Congrulation You are right");
+        window.location="quiz2.html";
+    }
+
+    if(from.test [2].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz1.html";
+    }
+
+    if(from.test [3].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz1.html";
+    }
+
+ 
+}
+
+// quiz2 start
+
+function hello(from2){
+    if(from2.test2 [0].checked){
+        alert("Congrulation You are right");
+        window.location="quiz3.html";
+    }
+
+    if( from2.test2[1].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz2.html";
+    }
+
+    if(from2.test2 [2].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz2.html";
+    }
+
+    if(from2.test2 [3].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz2.html";
+    }
+ 
+}
+
+// quiz2 start
+
+function next3(from3){
+    if(from3.test3 [0].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz3.html";
+    }
+
+    if( from3.test3[1].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz3.html";
+    }
+
+    if(from3.test3 [2].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz3.html";
+    }
+
+    if(from3.test3 [3].checked){
+        alert("Congrulation You are right");
+        window.location="quiz4.html";
+    }
+ 
+}
+
+// quiz4 start
+
+function next4(from4){
+    if(from4.test4 [0].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz4.html";
+    }
+
+    if( from4.test4[1].checked){
+        alert("Congrulation You are right");
+        window.location="quiz5.html";
+    }
+
+    if(from4.test4 [2].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz3.html";
+    }
+
+    if(from4.test4 [3].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz4.html";
+    }
+ 
+}
+
+// quiz5 start
+
+function next5(from5){
+    if(from5.test5 [0].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz5.html";
+    }
+
+    if( from5.test5[1].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz5.html";
+    }
+
+    if(from5.test5 [2].checked){
+        alert("Congrulation You are right");
+        window.location="win.html";
+    }
+
+    if(from5.test5 [3].checked){
+        alert("You are wrong sorry try again");
+        window.location="quiz5.html";
+    }
+ 
+}
+
+// winpagestart
+
+var x = prompt("write your name")
+
+ document.write(  " Congratulations "+ x);
+
+
+
+
